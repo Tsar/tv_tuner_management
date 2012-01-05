716bb1659b32344545d3a9d1a47696e6f73e5d9b
Ioann Volkov <volkov.ioann@gmail.com>
2012-01-05 23:54:42 +0300

    Hypothetically, this installer script for AverMedia MCE116 Plus may work

---
+++ tv_tuner_installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script installs soft to work with AverMedia MCE116 Plus (for XUbuntu 11.10)
+# Run as root or with "sudo"
+
+apt-get install firmware-ivtv ivtv-utils
+rmmod ivtv
+modprobe ivtv
+wget http://www.steventoth.net/linux/xc5000/HVR-12x0-14x0-17x0_1_25_25271_WHQL.zip  #if this URL doesn't work, comment this line and uncomment next
+#wget https://raw.github.com/Tsar/tv_tuner_management/master/files_for_installer/HVR-12x0-14x0-17x0_1_25_25271_WHQL.zip
+unzip -j HVR-12x0-14x0-17x0_1_25_25271_WHQL.zip Driver85/hcw85bda.sys
+wget http://linuxtv.org/hg/v4l-dvb/raw-file/3919b17dc88e/linux/Documentation/video4linux/extract_xc3028.pl  #if this URL doesn't work, comment this line and uncomment next
+#wget https://raw.github.com/Tsar/tv_tuner_management/master/files_for_installer/extract_xc3028.pl
+perl extract_xc3028.pl
+cp xc3028-v27.fw /lib/firmware/xc3028-v27.fw
+
+# Now you may launch "tv_tuner_channels_switcher.sh" and enjoy watching TV :)
